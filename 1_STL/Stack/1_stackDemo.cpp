c9f4432fbb6d42dba43f29ca6c41f51484fb1f70
itsme-swe <me.harshmehra07@gmail.com>
2025-01-17 22:50:47 +0530

    update

---
+++ 1_STL/Stack/1_stackDemo.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+
+  stack<int> st;
+
+  st.push(10);
+  st.push(12);
+  st.push(14);
+  st.push(16);
+  st.push(18);
+
+  while (!st.empty())
+  {
+    cout << st.top() << " ";
+    st.pop();
+  }
+  cout << endl;
+
+  st.pop();
+
+  while (!st.empty())
+  {
+    cout << st.top() << " ";
+    st.pop();
+  }
+  cout << endl;
+
+  return 0;
+}
