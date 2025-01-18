9e276289eb81ba3b3e1eabe81df0e509e070f1f1
itsme-swe <me.harshmehra07@gmail.com>
2025-01-18 09:07:08 +0530

    update

--- 1_STL/Map/4_mapMethod_findAndat.cpp
+++ 1_STL/Map/4_mapMethod_findAndat.cpp
@@ -7,6 +7,15 @@ int main()
 {
   map<char, int> mp = {{'A', 101}, {'B', 301}, {'D', 201}, {'C', 101}, {'E', 505}};
 
-  cout << mp.at('F') << endl; // Return the value if key is present otherwise throws an exception.
+  cout << mp.at('A') << endl; // Return the value if key is present otherwise throws an exception.
+
+  if (mp.find('B') != mp.end())
+  {
+    cout << "Key found" << endl;
+  }
+  else
+  {
+    cout << "Key not found" << endl;
+  }
   return 0;
 }
