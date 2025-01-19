19467199a5b96367fb46cce234ba7f58cf9822a2
itsme-swe <me.harshmehra07@gmail.com>
2025-01-19 21:58:15 +0530

    update

--- 3_Arrays/1D_Array/Ques_1DArray/ReverseAnArray.cpp
+++ 3_Arrays/1D_Array/Ques_1DArray/ReverseAnArray.cpp
@@ -7,10 +7,41 @@ using namespace std;
 
 void swap(int a[], int i, int j)
 {
+  int temp = a[i];
+  a[i] = a[j];
+  a[j] = temp;
+}
+
+void printArray(int arr[], int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
+void reverseArray(int arr[], int n)
+{
+  int low = 0;
+  int high = n - 1;
+
+  while (low < high)
+  {
+    swap(arr[low], arr[high]);
+    low++;
+    high--;
+  }
 }
 
 int main()
 {
+  int array[] = {1, 2, 3, 4, 5};
 
+  int n = sizeof(array) / sizeof(int);
+
+  reverseArray(array, n);
+
+  printArray(array, n);
   return 0;
 }
