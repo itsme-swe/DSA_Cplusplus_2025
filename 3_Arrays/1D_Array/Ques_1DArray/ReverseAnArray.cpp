a465593533a65c29528080e31195e6be63e6567f
itsme-swe <me.harshmehra07@gmail.com>
2025-01-19 21:14:39 +0530

    update

---
+++ 3_Arrays/1D_Array/Ques_1DArray/ReverseAnArray.cpp
@@ -0,0 +1,16 @@
+/*
+💥 To reverse an array we'll using "Two Pointer approach".
+*/
+#include <iostream>
+
+using namespace std;
+
+void swap(int a[], int i, int j)
+{
+}
+
+int main()
+{
+
+  return 0;
+}
